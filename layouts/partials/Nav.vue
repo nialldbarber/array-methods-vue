ad620c7615d903d024989ab874f9a6c36a8d4e69
Niall <nialldbarber@gmail.com>
2019-01-07 22:12:51 +0000

    Initial commit

---
+++ layouts/partials/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nuxt-link
+      to="/"
+      class="logo">Array Methods</nuxt-link>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/pop">Pop</nuxt-link>
+          <nuxt-link to="/push">Push</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
